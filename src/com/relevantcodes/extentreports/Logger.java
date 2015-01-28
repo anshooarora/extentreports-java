1e4f3289cba54d602c97e536431a9e8927512c71
anshooarora <anshooarora@gmail.com>
2015-01-28 16:32:24 -0600

    initial

--- src/com/relevantcodes/extentreports/Logger.java
+++ src/com/relevantcodes/extentreports/Logger.java
@@ -1,5 +1,5 @@
 /*
-Copyright 2015 Cube Reports committer(s)
+Copyright 2015 ExtentReports committer(s)
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
