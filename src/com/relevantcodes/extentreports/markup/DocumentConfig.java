cd676b48885b501f5bf01ac5535682cbf1f42fe5
anshooarora <anshooarora@gmail.com>
2015-04-22 10:08:53 -0500

    Added Masonry grid added [GridType.MASONRY] Ability to add labels to reports Add code-blocks to report via <pre></pre> tags Ability to change size of attached images [setImageSize] Hide the caller class using [displayCallerClass] Modify chart titles [chartTitle] Lots of styling improvements and even a more cleaner report Fixed bug where JavaScript does not get embedded when calling addCustomScripts()

--- src/com/relevantcodes/extentreports/markup/DocumentConfig.java
+++ src/com/relevantcodes/extentreports/markup/DocumentConfig.java
@@ -9,20 +9,26 @@
 
 package com.relevantcodes.extentreports.markup;
 
+import com.relevantcodes.extentreports.Chart;
+import com.relevantcodes.extentreports.LogSettings;
 import com.relevantcodes.extentreports.LogStatus;
+import com.relevantcodes.extentreports.support.FileReaderEx;
+import com.relevantcodes.extentreports.support.FileWriterEx;
+import com.relevantcodes.extentreports.support.RegexMatcher;
 
 /**
  * DocumentConfig class for customizing the report layout
  * 
  * @author Anshoo Arora
  */
-public class DocumentConfig {
+public class DocumentConfig extends LogSettings {
 	private Content content;
 	private DocumentHead docHead;
 	private Footer footer;
 	private Header header;
 	private Scripts scripts;
 	private String filePath;
+	
 	
 	/**
 	 *  Choose to use your own icons for log status (PASS, FAIL, WARNING etc.) 
@@ -31,7 +37,7 @@ public class DocumentConfig {
 	 *  
 	 * @param status LogStatus
 	 * @param newIcon Icon string from font-awesome
-	 * @return DocumentConfig object
+	 * @return DocumentConfig
 	 */
 	public DocumentConfig statusIcon(LogStatus status, String newIcon) {
 		FontAwesomeIco.override(status, newIcon);
@@ -43,8 +49,9 @@ public class DocumentConfig {
 	 * Allows showing of TH headers for the test
 	 * 
 	 * @param display Setting FALSE will remove the TH headers from the test table
-	 * @return DocumentConfig object
+	 * @return DocumentConfig
 	 */
+	@Deprecated
 	public DocumentConfig displayTestHeaders(Boolean display) {
 		if (!display)
 			return addCustomStyles("th { display: none; }");
@@ -57,7 +64,7 @@ public class DocumentConfig {
 	 * By default, this is FALSE
 	 * 
 	 * @param display Setting FALSE will hide the default Extent footer
-	 * @return DocumentConfig object
+	 * @return DocumentConfig
 	 */
 	public DocumentConfig useExtentFooter(Boolean display) {
 		if (null == footer)
@@ -72,7 +79,7 @@ public class DocumentConfig {
 	 * Adds custom scripts to the report
 	 * 
 	 * @param script JavaScript code to be used for the report
-	 * @return DocumentConfig object
+	 * @return DocumentConfig
 	 */
 	public DocumentConfig insertJS(String script) {
 		if (null == scripts)
@@ -87,7 +94,7 @@ public class DocumentConfig {
 	 * Add, remove or modify the report summary
 	 * 
 	 * @param headline Report summary
-	 * @return DocumentConfig object
+	 * @return DocumentConfig
 	 */
 	public DocumentConfig reportHeadline(String headline) {
 		if (null == header)
@@ -102,12 +109,27 @@ public class DocumentConfig {
 		
 		return this;
 	}
+	
+	/**
+	 * Add, remove or modify the report summary
+	 * 
+	 * @param title Title of the report
+	 * @return DocumentConfig
+	 */
+	public DocumentConfig reportTitle(String title) {
+		if (null == header)
+	        header = new Header();
+
+		header.setFile(filePath).setTitle(title);
+		
+		return addCustomStyles(".topbar-items-left { display: inline-block; }");
+	}
 
 	/**
 	 * Use a custom stylesheet file with CSS
 	 * 
 	 * @param cssFilePath Path of the CSS stylesheet file
-	 * @return DocumentConfig object
+	 * @return DocumentConfig
 	 */
 	public DocumentConfig addCustomStylesheet(String cssFilePath) {
 		if (null == docHead)
@@ -122,12 +144,12 @@ public class DocumentConfig {
 	 * Use custom CSS directly for the document to change layout
 	 * 
 	 * @param styles CSS style string
-	 * @return DocumentConfig object
+	 * @return DocumentConfig
 	 */ 
 	public DocumentConfig addCustomStyles(String styles) {
 		if (null == docHead)
 	        docHead = new DocumentHead();
-		
+			
 		docHead.setFile(filePath).addCustomStyles(styles);
 		
 		return this;
@@ -137,7 +159,7 @@ public class DocumentConfig {
 	 * Change the title of the report file
 	 * 
 	 * @param title Document title
-	 * @return DocumentConfig object
+	 * @return DocumentConfig
 	 */
 	public DocumentConfig documentTitle(String title) {
 		if (null == content)
@@ -160,6 +182,55 @@ public class DocumentConfig {
 		return this;
 	}
 	
+	/**
+	 * Setting to append the caller class to the log event. By default, the setting is enabled 
+	 * to allow the caller class to be displayed
+	 * 
+	 * @param display Shows the caller class in the report step
+	 * @return DocumentConfig
+	 */
+	public DocumentConfig displayCallerClass(Boolean display) {
+		displayCallerClass = display;
+		
+		return this;
+	}
+	
+	/**
+	 * Sets the size of the attached snapshots by percentage. Default = 50%.
+	 * 
+	 * <p>
+	 * <b>Usage: </b>
+	 * 		extent.setImageSize("40%");
+	 * 
+	 * @param percent Size of the image in percentage
+	 * @return DocumentConfig
+	 */
+	public DocumentConfig setImageSize(String percent) {
+		if (percent.substring(percent.length() - 1).equals("%"))
+			return addCustomStyles(".report-img, .report-img-large { width:" + percent + ";}");
+			
+		return this;
+	}
+	
+	/**
+	 * Allows changing the title of the dashboard charts
+	 * 
+	 * @param chart TEST_SET, TEST
+	 * @param title New title of the chart
+	 * @return DocumentConfig
+	 */
+	public DocumentConfig chartTitle(Chart chart, String title) {
+		String html = FileReaderEx.readAllText(filePath);
+		String currentTitle = MarkupFlag.get(chart.toString() + "chartTitle", "js", true) + ".*" + MarkupFlag.get(chart.toString() + "chartTitle", "js", true);
+		String match = RegexMatcher.getNthMatch(html, currentTitle, 0);
+		
+		html = html.replace(match, MarkupFlag.get(chart.toString() + "chartTitle", "js", false) + "'" + title + "'" + MarkupFlag.get(chart.toString() + "chartTitle", "js", false));
+		
+		FileWriterEx.write(filePath, html);
+		
+		return this;
+	}
+		
 	/**
 	 * @param filePath Path of the HTML file with .htm or .html extension
 	 */
