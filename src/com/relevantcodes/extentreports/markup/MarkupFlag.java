8c9c581d03534902e1c82c3dd5b60cdca62eb45d
anshooarora <anshooarora@gmail.com>
2015-04-22 10:07:59 -0500

    Added Masonry grid added [GridType.MASONRY] Ability to add labels to reports Add code-blocks to report via <pre></pre> tags Ability to change size of attached images [setImageSize] Hide the caller class using [displayCallerClass] Modify chart titles [chartTitle] Lots of styling improvements and even a more cleaner report Fixed bug where JavaScript does not get embedded when calling addCustomScripts()

--- src/com/relevantcodes/extentreports/markup/MarkupFlag.java
+++ src/com/relevantcodes/extentreports/markup/MarkupFlag.java
@@ -14,11 +14,29 @@ public class MarkupFlag {
 		return "<!--%%" + flag.toUpperCase() + "%%-->";
 	}
 	
+	public static String get(String flag, String type, Boolean forRegex) {
+		if (type == "js" && forRegex)
+			return "/\\*" + flag.toUpperCase() + "\\*/";
+		
+		if (type == "js")
+			return "/*" + flag.toUpperCase() + "*/";
+			
+		return "<!--%%" + flag.toUpperCase() + "%%-->";
+	}
+	
 	public static String img(String imgPath) {
 		return "<img class='report-img' data-featherlight='file:///" + imgPath + "' src='file:///" + imgPath + "' />";
 	}
 	
 	public static String imgSingle(String imgPath) {
 		return "<img class='report-img-large' data-featherlight='file:///" + imgPath + "' src='file:///" + imgPath + "' />";
+	}
+	
+	public static String createSingleNode(String nodeName, String className, String text) {
+		return "<" + nodeName + " class='" + className + "'>" + text + "</" + nodeName + ">";
+	}
+	
+	public static String createSingleNode(String nodeName, String className) {
+		return createSingleNode(nodeName, className, "");
 	}
 }
