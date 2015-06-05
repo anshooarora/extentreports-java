551ad04b734fa36e261a04802596eeae5c9e220c
anshooarora <anshooarora@gmail.com>
2015-06-05 13:08:44 -0500

    displayCallerClass obsolete

--- src/com/relevantcodes/extentreports/LogSettings.java
+++ src/com/relevantcodes/extentreports/LogSettings.java
@@ -10,7 +10,6 @@
 package com.relevantcodes.extentreports;
 
 public abstract class LogSettings {
-	protected static Boolean displayCallerClass = true;
 	protected static String logTimeFormat = "hh:mm:ss";
 	protected static String logDateFormat = "mm-dd-yyyy";
 	protected static String logDateTimeFormat = logDateFormat + " " + logTimeFormat;
