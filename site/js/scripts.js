e662eaa0b31fd21f41ea3cf2d2688026222aa6a1
anshooarora <anshooarora@gmail.com>
2015-07-06 11:15:45 -0500

    Added new docs for .NET

--- site/js/scripts.js
+++ site/js/scripts.js
@@ -1,15 +1,22 @@
 $(document).ready(function() {
-	hljs.initHighlightingOnLoad();
-	$(document).ready(sizing);
-	$(window).resize(sizing);
-	function sizing() {
-		if ($(window).width() > 1650) {
-			$('.top-unit').removeClass('unit-80').addClass('unit-70');
-		} 
-		else {
-			$('.top-unit').removeClass('unit-70').addClass('unit-80');
-		}
-	}
+    hljs.initHighlightingOnLoad();
+    $('.menu-items').dropit();
+    $(document).ready(sizing);
+    $(window).resize(sizing);
+    function sizing() {
+        if ($(window).width() > 1650) {
+            $('.top-unit').removeClass('unit-80').addClass('unit-70');
+        } 
+        else {
+            $('.top-unit').removeClass('unit-70').addClass('unit-80');
+        }
+    }
+    $('.logo').mouseover(function() {
+        $(this).addClass('animated hinge');
+        setTimeout(function() {
+            $('.logo').removeClass('animated hinge').show();
+        }, 2000);
+    });
 });
 var sc_project=7899149; var sc_invisible=1; var sc_security="75b043e9"; 
 var scJsHost = (("https:" == document.location.protocol) ? "https://secure." : "http://www.");
