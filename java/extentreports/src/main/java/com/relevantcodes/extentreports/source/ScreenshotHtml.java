82e740699f5919cb71f174b7a0cbfcfd469b69e2
anshooarora <anshooarora@gmail.com>
2015-09-30 10:21:14 -0500

    Java 2.10 - Beta 5
    fixed issue with screenshot enlarge view not showing correctly
    fixed issue with reportHeadline
    many other minor js fixes

--- java/extentreports/src/main/java/com/relevantcodes/extentreports/source/ScreenshotHtml.java
+++ java/extentreports/src/main/java/com/relevantcodes/extentreports/source/ScreenshotHtml.java
@@ -10,6 +10,6 @@ package com.relevantcodes.extentreports.source;
 
 public class ScreenshotHtml {
     public static String getSource(String imgPath) {
-        return "<img class='report-img materialboxed' src='file:///" + imgPath + "' />";
+    	return "<img class='report-img' data-featherlight='file:///" + imgPath + "' src='file:///" + imgPath + "' />";
     }
 }
