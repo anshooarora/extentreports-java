b94b8d080e0f4f528a0a71216dbd68f7502e91f6
Anshoo Arora <anshooarora@gmail.com>
2016-10-30 00:49:38 -0500

    new listener example tests

---
+++ src/test/java/com/aventstack/extentreports/common/ExtentTestNGIReporterListener.java
@@ -0,0 +1,94 @@
+package com.aventstack.extentreports.common;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.testng.IReporter;
+import org.testng.IResultMap;
+import org.testng.ISuite;
+import org.testng.ISuiteResult;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+import org.testng.xml.XmlSuite;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.ChartLocation;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+public class ExtentTestNGIReporterListener implements IReporter {
+    
+    private static final String OUTPUT_FOLDER = "test-output/";
+    private static final String FILE_NAME = "Extent.html";
+    
+    private ExtentReports extent;
+
+    @Override
+    public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
+        init();
+        
+        for (ISuite suite : suites) {
+            Map<String, ISuiteResult> result = suite.getResults();
+            
+            for (ISuiteResult r : result.values()) {
+                ITestContext context = r.getTestContext();
+                
+                buildTestNodes(context.getFailedTests(), Status.FAIL);
+                buildTestNodes(context.getSkippedTests(), Status.SKIP);
+                buildTestNodes(context.getPassedTests(), Status.PASS);
+                
+            }
+        }
+        
+        for (String s : Reporter.getOutput()) {
+            extent.setTestRunnerOutput(s);
+        }
+        
+        extent.flush();
+    }
+    
+    private void init() {
+        ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(OUTPUT_FOLDER + FILE_NAME);
+        htmlReporter.config().setDocumentTitle("ExtentReports - Created by TestNG Listener");
+        htmlReporter.config().setReportName("ExtentReports - Created by TestNG Listener");
+        htmlReporter.config().setTestViewChartLocation(ChartLocation.BOTTOM);
+        htmlReporter.config().setTheme(Theme.STANDARD);
+        
+        extent = new ExtentReports();
+        extent.attachReporter(htmlReporter);
+    }
+    
+    private void buildTestNodes(IResultMap tests, Status status) {
+        ExtentTest test;
+        
+        if (tests.size() > 0) {
+            for (ITestResult result : tests.getAllResults()) {
+                test = extent.createTest(result.getMethod().getMethodName());
+                
+                for (String group : result.getMethod().getGroups())
+                    test.assignCategory(group);
+
+                if (result.getThrowable() != null) {
+                    test.log(status, result.getThrowable());
+                }
+                else {
+                    test.log(status, "Test " + status.toString().toLowerCase() + "ed");
+                }
+                
+                test.getModel().setStartTime(getTime(result.getStartMillis()));
+                test.getModel().setEndTime(getTime(result.getEndMillis()));
+            }
+        }
+    }
+    
+    private Date getTime(long millis) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(millis);
+        return calendar.getTime();      
+    }
+}
