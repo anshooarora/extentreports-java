5e0f152827eda62d0113b09151ed1a55ffd5c300
Anshoo Arora <anshooarora@gmail.com>
2017-09-11 11:02:59 -0400

    fix #947 - issue where base64 was causing NPE while uploading to ExtentX

--- src/main/java/com/aventstack/extentreports/mediastorage/HttpMediaManager.java
+++ src/main/java/com/aventstack/extentreports/mediastorage/HttpMediaManager.java
@@ -90,6 +90,9 @@ class HttpMediaManager implements MediaStorage {
     
     @Override
     public void storeMedia(Media m) throws IOException {
+        if (m.getBase64String() != null)
+            return;
+            
         File f = new File(m.getPath());
         if (!f.exists()) {
             throw new IOException("The system cannot find the file specified " + m.getPath());
